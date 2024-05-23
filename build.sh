d9a0708bcf19082d8ca243de95659248b5047334
Roel <10514742+rboonzaijer@users.noreply.github.com>
2024-05-23 12:29:06 +0200

    build without cache

--- build.sh
+++ build.sh
@@ -5,18 +5,18 @@ docker images -a | grep "^roelscript/*" | awk '{print $1":"$2}' | xargs docker r
 
 #######################################
 # Build images
-docker build -f ./alpine/rootless/Dockerfile -t roelscript/img:alpine .
-docker build -f ./alpine/root/Dockerfile -t roelscript/img:alpine-root .
-docker build -f ./alpine/nobody/Dockerfile -t roelscript/img:alpine-nobody .
+docker build --no-cache -f ./alpine/rootless/Dockerfile -t roelscript/img:alpine .
+docker build --no-cache -f ./alpine/root/Dockerfile -t roelscript/img:alpine-root .
+docker build --no-cache -f ./alpine/nobody/Dockerfile -t roelscript/img:alpine-nobody .
 
-docker build -f ./imagemagick/Dockerfile -t roelscript/img:imagemagick .
+docker build --no-cache -f ./imagemagick/Dockerfile -t roelscript/img:imagemagick .
 
-docker build -f ./nginx/Dockerfile -t roelscript/img:nginx .
+docker build --no-cache -f ./nginx/Dockerfile -t roelscript/img:nginx .
 
-docker build -f ./php-nginx/8.3/Dockerfile -t roelscript/img:php-nginx-8.3 .
-docker build -f ./php-nginx/8.3-dev/Dockerfile -t roelscript/img:php-nginx-8.3-dev .
-docker build -f ./php-nginx/8.2/Dockerfile -t roelscript/img:php-nginx-8.2 .
-docker build -f ./php-nginx/8.2-dev/Dockerfile -t roelscript/img:php-nginx-8.2-dev .
+docker build --no-cache -f ./php-nginx/8.3/Dockerfile -t roelscript/img:php-nginx-8.3 .
+docker build --no-cache -f ./php-nginx/8.3-dev/Dockerfile -t roelscript/img:php-nginx-8.3-dev .
+docker build --no-cache -f ./php-nginx/8.2/Dockerfile -t roelscript/img:php-nginx-8.2 .
+docker build --no-cache -f ./php-nginx/8.2-dev/Dockerfile -t roelscript/img:php-nginx-8.2-dev .
 
 
 #######################################
